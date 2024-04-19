65e4b9489a5c2c92b57d13dee4914927467bba6c
slow-life-pg <slow.life.pg@gmail.com>
2024-04-19 23:13:19 +0900

    実行準備まで実装

--- src/main.py
+++ src/main.py
@@ -1,20 +1,38 @@
-import sys
-import json
-from pathlib import Path
+if __name__ == "__main__":
+    import sys
+    import json
+    from pathlib import Path
+    from datetime import datetime
 
-with open("config.json", encoding="utf-8") as fc:
-    config = json.load(fc)
+    toolDir = Path(__file__).parent
+    print("****************************************")
+    print(f"VBAUnit kicked on {Path.cwd()}")
+    print(f"Tool is in {toolDir}")
+    print(f"{datetime.now()}")
+    print()
 
-print(f"start with: {config['scenario']}")
+    configPath = toolDir.joinpath("config.json")
 
-scenarioPath = Path(config["scenario"])
-if scenarioPath.is_absolute():
-    print("absolute path")
-else:
-    print(f"relative path based on {Path.cwd()}")
-    scenarioPath = scenarioPath.resolve()
-    print(f"resolved: {scenarioPath}")
+    with open(str(configPath), encoding="utf-8") as fc:
+        config = json.load(fc)
 
-if not scenarioPath.exists():
-    print("scenario not exists")
-    sys.exit()
+    print(f"start with: {config['scenario']}")
+
+    scenarioPath = Path(config["scenario"])
+    if scenarioPath.is_absolute():
+        print("absolute path")
+    else:
+        print(f"relative path based on {Path.cwd()}")
+        scenarioPath = scenarioPath.resolve()
+        print(f"resolved: {scenarioPath}")
+
+    if not scenarioPath.exists():
+        print("scenario not exists")
+        sys.exit()
+
+    print()
+    print("run test!")
+
+    bridgePath = toolDir.joinpath("Bridge.xlsm")
+
+    print(f"using: {bridgePath}")
