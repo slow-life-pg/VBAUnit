0101b25969fba69facb864ce0c2c02d13e6727a6
slow-life-pg <slow.life.pg@gmail.com>
2025-08-30 17:28:36 +0900

    ライブラリを実装

--- src/vbaunit_lib/testlib.py
+++ src/vbaunit_lib/testlib.py
@@ -18,9 +18,13 @@ class VBAUnitTestLib:
         else:
             self.__app = None
         self.__book = None
+        self.__internalbookname = ""
 
     def exitapp(self) -> None:
         self.closeexcel()
+        self.__closeapp()
+
+    def __closeapp(self) -> None:
         if self.__app is not None:
             self.__app.quit()
             self.__app = None
@@ -33,8 +37,10 @@ class VBAUnitTestLib:
         self.__app.screen_updating = False
 
         self.__book = self.__app.books.open(self.__bridgepath)
+        # 開けなかったらErrorが出ているはずだから独自にthrowしない。
         if self.__book:
             self.__book.api.VBProject.References.AddFromFile(excelpath)
+            self.__internalbookname = Path(excelpath).name
 
         return self.__book
 
@@ -42,32 +48,55 @@ class VBAUnitTestLib:
         if self.__book is not None:
             self.__book.close()
             self.__book = None
+            if self.__withapp:
+                self.__closeapp()
 
     def getregexobj(self) -> object:
         """bridgeからRegexを取得"""
-        pass
+        if self.__book:
+            vbamacro = self.__book.macro("GetRegexp")
+            regexobj = vbamacro()
+            return regexobj
+        else:
+            return None
 
     def getcollectionobj(self) -> object:
         """bridgeからCollectionを取得"""
-        pass
+        if self.__book:
+            vbamacro = self.__book.macro("GetNewCollection")
+            collectionobj = vbamacro()
+            return collectionobj
+        else:
+            return None
 
     def getdictionaryobj(self) -> object:
         """bridgeからDictionaryを取得"""
-        pass
+        if self.__book:
+            vbamacro = self.__book.macro("GetNewDictionary")
+            dictionaryobj = vbamacro()
+            return dictionaryobj
+        else:
+            return None
 
     def freeobj(self, obj: object) -> None:
         """bridgeから取得したオブジェクトを解放"""
-        pass
+        if self.__book:
+            vbamacro = self.__book.macro("Free")
+            vbamacro(obj)
 
     def callmacro(self, obj: object, macro_name: str, *args) -> object:
         """bridgeからマクロを呼び出す"""
         if self.__book:
             if len(args) <= 16:
                 vbamacro = self.__book.macro(macro_name)
-                res: list[object] = vbamacro(*args)
+                res: list[object] = vbamacro(
+                    obj, self.__internalbookname, macro_name, *args
+                )
                 return res[0] if res else None
             else:
-                print(f"callmacro: too many macro arguments {len(args)}")
+                print(
+                    f"callmacro: too many macro arguments {len(args)}. Must be <= 16."
+                )
                 return None
         else:
             print("callmacro: no book has opened")
