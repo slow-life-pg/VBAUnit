1cbea820f27991fa415ca1d15169d6aea3145dd2
slow-life-pg <slow.life.pg@gmail.com>
2025-09-20 18:46:21 +0900

    testablity向上

--- src/runner/run.py
+++ src/runner/run.py
@@ -3,6 +3,8 @@ import shutil
 import json
 from datetime import datetime
 from openpyxl import load_workbook
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 from natsort import natsort_keygen
 from util.types import TestSuite, TestModule, TestCase, TestResult
 
@@ -47,8 +49,86 @@ def __createresult(testcase: TestCase, succeeded: bool) -> TestResult:
     return result
 
 
-def __runtestcase(testcase: TestCase) -> TestResult:
+def __runtestcase(testcase: TestCase, f) -> TestResult:
     return __createresult(testcase=testcase, succeeded=False)
+
+
+def __setmoduleresults(
+    testcase: TestCase,
+    result: TestResult,
+    modulelist: list[TestModule],
+    modulesummary_success: dict[str, int],
+    modulesummary_failure: dict[str, int],
+) -> None:
+    modulename = testcase.module.modulepath.name
+    if modulename not in modulesummary_success:
+        modulesummary_success[modulename] = 0
+        modulesummary_failure[modulename] = 0
+        modulelist.append(testcase.module)
+    if result.succeeded:
+        modulesummary_success[modulename] += 1
+    else:
+        modulesummary_failure[modulename] += 1
+
+
+def __runtestsuite(
+    suite: TestSuite,
+    results: list[TestResult],
+    modulelist: list[TestModule],
+    f,
+    modulesummary_success: dict[str, int],
+    modulesummary_failure: dict[str, int],
+) -> None:
+    for testcase in suite:
+        __writestartlog(testcase=testcase, f=f)
+
+        try:
+            result = __runtestcase(testcase=testcase, f=f)
+        except Exception as e:
+            print(f"Runtime error: {e}")
+            result = __createresult(testcase=testcase, succeeded=False)
+
+        __writeendlog(result=result, f=f)
+        f.flush()
+
+        results.append(result)
+        __setmoduleresults(
+            testcase=testcase,
+            result=result,
+            modulelist=modulelist,
+            modulesummary_success=modulesummary_success,
+            modulesummary_failure=modulesummary_failure,
+        )
+
+
+def __writemodulersult(
+    resultbook: Workbook, modulelist: list[TestModule], modulesummary_success: dict[str, int], modulesummary_failure: dict[str, int]
+) -> None:
+    for module in modulelist:
+        modulename = module.modulepath.name
+        resultsheet = resultbook[module.group]
+        if modulesummary_success[modulename] >= 0 and modulesummary_failure[modulename] == 0:
+            resultsheet.cell(row=module.line, column=6, value="○")
+        elif modulesummary_success[modulename] > 0 and modulesummary_failure[modulename] > 0:
+            resultsheet.cell(row=module.line, column=6, value="△")
+        else:
+            resultsheet.cell(row=module.line, column=6, value="×")
+
+
+def __writeresults(resultsheet: Worksheet, results: list[TestResult]) -> None:
+    max_row = 1
+    rkey = natsort_keygen()
+    for result in sorted(results, key=lambda r: (rkey(r.testid), r.testfunction)):
+        resultdump = {
+            "testid": result.testid,
+            "module": result.module.modulepath.name,
+            "function": result.testfunction,
+            "line": result.start_line,
+            "succeeded": result.succeeded,
+            "runned_at": result.runned_at,
+        }
+        newsheet.cell(row=max_row, column=1, value=json.dumps(resultdump))
+        max_row += 1
 
 
 def run_testsuite(suite: TestSuite, scenario: Path, bridge: Path, out: Path) -> None:
@@ -77,29 +157,16 @@ def run_testsuite(suite: TestSuite, scenario: Path, bridge: Path, out: Path) -> 
     modulesummary_success: dict[str, int] = {}
     modulesummary_failure: dict[str, int] = {}
     with open(testlogpath, mode="a", encoding="utf-8") as f:
-        for testcase in suite:
-            __writestartlog(testcase=testcase, f=f)
-
-            try:
-                result = __runtestcase(testcase=testcase)
-            except Exception as e:
-                print(f"Runtime error: {e}")
-                result = __createresult(testcase=testcase, succeeded=False)
-
-            __writeendlog(result=result, f=f)
-            f.flush()
-
-            results.append(result)
-
-            modulename = testcase.module.modulepath.name
-            if modulename not in modulesummary_success:
-                modulesummary_success[modulename] = 0
-                modulesummary_failure[modulename] = 0
-                modulelist.append(testcase.module)
-            if result.succeeded:
-                modulesummary_success[modulename] += 1
-            else:
-                modulesummary_failure[modulename] += 1
+        f.write(f"\nStart at: {__gettimestampnow()}\n")
+        __runtestsuite(
+            suite=suite,
+            results=results,
+            modulelist=modulelist,
+            f=f,
+            modulesummary_success=modulesummary_success,
+            modulesummary_failure=modulesummary_failure,
+        )
+        f.write(f"\nFinished at: {__gettimestampnow()}\n")
 
     # 結果の書込
 
@@ -108,34 +175,17 @@ def run_testsuite(suite: TestSuite, scenario: Path, bridge: Path, out: Path) -> 
         resultbook = load_workbook(outputpath)
 
         # モジュールの結果
-        for module in modulelist:
-            modulename = module.modulepath.name
-            resultsheet = resultbook[module.group]
-            if modulesummary_success[modulename] >= 0 and modulesummary_failure[modulename] == 0:
-                resultsheet.cell(row=module.line, column=6, value="○")
-            elif modulesummary_success[modulename] > 0 and modulesummary_failure[modulename] > 0:
-                resultsheet.cell(row=module.line, column=6, value="△")
-            else:
-                resultsheet.cell(row=module.line, column=6, value="×")
+        __writemodulersult(
+            resultbook=resultbook,
+            modulelist=modulelist,
+            modulesummary_success=modulesummary_success,
+            modulesummary_failure=modulesummary_failure,
+        )
 
         # テストケースの結果
-        rkey = natsort_keygen()
-
         newsheetname = f"Results_{datetime.now():%Y%m%d_%H%M%S}"
         newsheet = resultbook.create_sheet(newsheetname)
-        max_row = 1
-
-        for result in sorted(results, key=lambda r: (rkey(r.testid), r.testfunction)):
-            resultdump = {
-                "testid": result.testid,
-                "module": result.module.modulepath.name,
-                "function": result.testfunction,
-                "line": result.start_line,
-                "succeeded": result.succeeded,
-                "runned_at": result.runned_at,
-            }
-            newsheet.cell(row=max_row, column=1, value=json.dumps(resultdump))
-            max_row += 1
+        __writeresults(resultsheet=newsheet, results=results)
 
         resultbook.save(outputpath)
     finally:
